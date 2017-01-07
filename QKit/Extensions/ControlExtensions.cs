6d2e6fa2b47bf5655e43f3ed6386c6dec499e266
Quoc-Thy <Quoc-Thy@Q-NOTEBOOK>
2017-01-07 17:53:18 -0500

    Added attached property to set focus on when control is made visible

---
+++ QKit/Extensions/ControlExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace QKit.Extensions
+{
+    public class ControlExtensions
+    {
+        public static readonly DependencyProperty FocusWhenMadeVisibleProperty =
+            DependencyProperty.RegisterAttached("FocusWhenMadeVisible",
+                typeof(bool),
+                typeof(ControlExtensions),
+                new PropertyMetadata(false, IsFocusedPropertyChanged));
+
+        private static readonly DependencyProperty FocusWhenMadeVisibleCallbackTokenProperty =
+            DependencyProperty.RegisterAttached("FocusWhenMadeVisibleCallbackToken",
+                typeof(long?),
+                typeof(ControlExtensions),
+                new PropertyMetadata(null));
+
+        public static bool GetFocusWhenMadeVisible(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(FocusWhenMadeVisibleProperty);
+        }
+
+        public static void SetFocusWhenMadeVisible(DependencyObject obj, bool value)
+        {
+            obj.SetValue(FocusWhenMadeVisibleProperty, value);
+        }
+
+        private static long? GetFocusWhenMadeVisibleCallbackToken(DependencyObject obj)
+        {
+            return (long?)obj.GetValue(FocusWhenMadeVisibleCallbackTokenProperty);
+        }
+
+        private static void SetFocusWhenMadeVisibleCallbackToken(DependencyObject obj, long? value)
+        {
+            obj.SetValue(FocusWhenMadeVisibleCallbackTokenProperty, value);
+        }
+
+        private static void IsFocusedPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var control = obj as Control;
+
+            if (control == null)
+                return;
+
+            var token = GetFocusWhenMadeVisibleCallbackToken(control);
+
+            if (token.HasValue)
+                control.UnregisterPropertyChangedCallback(UIElement.VisibilityProperty, token.Value);
+
+            if ((bool)e.NewValue)
+            {
+                var newToken = control.RegisterPropertyChangedCallback(UIElement.VisibilityProperty, VisibilityChangedCallback);
+                SetFocusWhenMadeVisibleCallbackToken(control, newToken);
+            }
+        }
+
+        private static void VisibilityChangedCallback(DependencyObject sender, DependencyProperty dp)
+        {
+            var control = sender as Control;
+
+            if (control == null)
+                return;
+
+            if (control.Visibility == Visibility.Visible)
+            {
+                var isSuccessful = control.Focus(FocusState.Programmatic);
+
+                if (!isSuccessful)
+                    control.SizeChanged += Control_SizeChanged;
+            }
+        }
+
+        private static void Control_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var control = sender as Control;
+
+            if (control == null)
+                return;
+
+            var isSuccessful = control.Focus(FocusState.Programmatic);
+
+            if (isSuccessful)
+                control.SizeChanged -= Control_SizeChanged;
+        }
+    }
+}
--- QKitTestApp/ViewModels/MainPageViewModel.cs
+++ QKitTestApp/ViewModels/MainPageViewModel.cs
@@ -6,11 +6,24 @@ using Template10.Mvvm;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
 using QKit.Controls;
+using Windows.UI.Xaml;
 
 namespace QKitTestApp.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private Visibility _visibility = Visibility.Collapsed;
+
+        public Visibility Visibility
+        {
+            get { return _visibility; }
+            set { Set(ref _visibility, value); }
+        }
+
+        public void Toggle_Click()
+        {
+            Visibility = Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
 
         public void MasterDetails_ViewStateChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
