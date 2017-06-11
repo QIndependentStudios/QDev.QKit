ee849bc851f2f7e0c2c105d09828d136ead2055f
Quoc-Thy <Quoc-Thy@Q-NOTEBOOK>
2017-06-11 15:21:33 -0400

    Added custom title bar control.

---
+++ QKit/Controls/CustomTitleBar.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.System.Profile;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace QKit.Controls
+{
+    [TemplatePart(Name = BackgroundBrushPresenterName, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = InactiveBackgroundBrushPresenterName, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = XamlBackButtonName, Type = typeof(ButtonBase))]
+    public class CustomTitleBar : ContentControl
+    {
+        #region Events
+        public event EventHandler BackRequested;
+        #endregion
+
+        #region Fields
+        public const string BackgroundBrushPresenterName = "BackgroundBrushPresenter";
+        public const string InactiveBackgroundBrushPresenterName = "InactiveBackgroundBrushPresenter";
+        public const string XamlBackButtonName = "XamlBackButton";
+
+        private FrameworkElement BackgroundBrushPresenter;
+        private FrameworkElement InactiveBackgroundBrushPresenter;
+        private ButtonBase XamlBackButton;
+        #endregion
+
+        #region DependencyProperties
+        public static readonly DependencyProperty XamlBackButtonVisibilityProperty =
+            DependencyProperty.Register(nameof(XamlBackButtonVisibility),
+                typeof(Visibility),
+                typeof(CustomTitleBar),
+                new PropertyMetadata(Visibility.Collapsed));
+
+        public static readonly DependencyProperty LeftInsetVisibilityProperty = DependencyProperty.Register(
+            nameof(LeftInsetVisibility),
+            typeof(Visibility),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(Visibility.Visible));
+
+        public static readonly DependencyProperty LeftInsetWidthProperty = DependencyProperty.Register(
+            nameof(LeftInsetWidth),
+            typeof(double),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(default(double)));
+
+        public static readonly DependencyProperty RightInsetWidthProperty = DependencyProperty.Register(
+            nameof(RightInsetWidth),
+            typeof(double),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(default(double)));
+
+        public static readonly DependencyProperty InactiveBackgroundBrushProperty = DependencyProperty.Register(
+            nameof(InactiveBackgroundBrush),
+            typeof(Brush),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(default(Brush)));
+
+        public static readonly DependencyProperty InactiveBorderBrushProperty = DependencyProperty.Register(
+            nameof(InactiveBorderBrush),
+            typeof(Brush),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(default(Brush)));
+
+        public static readonly DependencyProperty AutoHideProperty = DependencyProperty.Register(
+            nameof(AutoHide),
+            typeof(bool),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(default(bool)));
+
+        public static readonly DependencyProperty NavigationFrameProperty = DependencyProperty.Register(
+            nameof(NavigationFrame),
+            typeof(Frame),
+            typeof(CustomTitleBar),
+            new PropertyMetadata(null));
+        #endregion
+
+        #region Constructors
+        public CustomTitleBar()
+        {
+            DefaultStyleKey = typeof(CustomTitleBar);
+
+            if (DesignMode.DesignModeEnabled)
+                return;
+
+            Window.Current.Activated += Current_Activated;
+
+            CoreApplication.GetCurrentView().TitleBar.IsVisibleChanged += TitleBar_IsVisibleChanged;
+            CoreApplication.GetCurrentView().TitleBar.LayoutMetricsChanged += TitleBar_LayoutMetricsChanged;
+
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.IoT")
+                XamlBackButtonVisibility = Visibility.Visible;
+        }
+        #endregion
+
+        #region Properties
+        public Visibility XamlBackButtonVisibility
+        {
+            get { return (Visibility)GetValue(XamlBackButtonVisibilityProperty); }
+            protected set { SetValue(XamlBackButtonVisibilityProperty, value); }
+        }
+
+        public Visibility LeftInsetVisibility
+        {
+            get { return (Visibility)GetValue(LeftInsetVisibilityProperty); }
+            protected set { SetValue(LeftInsetVisibilityProperty, value); }
+        }
+
+        public double LeftInsetWidth
+        {
+            get { return (double)GetValue(LeftInsetWidthProperty); }
+            protected set { SetValue(LeftInsetWidthProperty, value); }
+        }
+
+        public double RightInsetWidth
+        {
+            get { return (double)GetValue(RightInsetWidthProperty); }
+            protected set { SetValue(RightInsetWidthProperty, value); }
+        }
+
+        public Brush InactiveBackgroundBrush
+        {
+            get { return (Brush)GetValue(InactiveBackgroundBrushProperty); }
+            set { SetValue(InactiveBackgroundBrushProperty, value); }
+        }
+
+        public Brush InactiveBorderBrush
+        {
+            get { return (Brush)GetValue(InactiveBorderBrushProperty); }
+            set { SetValue(InactiveBorderBrushProperty, value); }
+        }
+
+        public bool AutoHide
+        {
+            get { return (bool)GetValue(AutoHideProperty); }
+            set { SetValue(AutoHideProperty, value); }
+        }
+
+        public Frame NavigationFrame
+        {
+            get { return (Frame)GetValue(NavigationFrameProperty); }
+            set { SetValue(NavigationFrameProperty, value); }
+        }
+
+        #endregion
+
+        #region Methods
+        protected override void OnApplyTemplate()
+        {
+            BackgroundBrushPresenter = GetTemplateChild(BackgroundBrushPresenterName) as FrameworkElement;
+            InactiveBackgroundBrushPresenter = GetTemplateChild(InactiveBackgroundBrushPresenterName) as FrameworkElement;
+            XamlBackButton = GetTemplateChild(XamlBackButtonName) as ButtonBase;
+
+            if (XamlBackButton != null)
+                XamlBackButton.Click += XamlBackButton_Click;
+
+            base.OnApplyTemplate();
+        }
+
+        private void UpdateTitleBarLayout(bool isTitleBarVisible)
+        {
+            var visibility = isTitleBarVisible ? Visibility.Visible : Visibility.Collapsed;
+
+            if (AutoHide)
+            {
+                LeftInsetVisibility = Visibility.Visible;
+                Visibility = visibility;
+            }
+            else
+            {
+                LeftInsetVisibility = visibility;
+            }
+        }
+
+        protected void OnBackRequested()
+        {
+            BackRequested?.Invoke(this, new EventArgs());
+        }
+        #endregion
+
+        #region Event Handlers
+        private void TitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            MinHeight = sender.Height;
+            LeftInsetWidth = sender.SystemOverlayLeftInset;
+            RightInsetWidth = sender.SystemOverlayRightInset;
+
+            UpdateTitleBarLayout(sender.IsVisible);
+        }
+
+        private void TitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender.IsVisible);
+        }
+
+        private void Current_Activated(object sender, WindowActivatedEventArgs e)
+        {
+            var isWindowActive = e.WindowActivationState != CoreWindowActivationState.Deactivated;
+            var backgroundBrushPresenterVisibility = isWindowActive ? Visibility.Visible : Visibility.Collapsed;
+            var backgroundInactiveBrushPresenterVisibility = isWindowActive ? Visibility.Collapsed : Visibility.Visible;
+
+            if (BackgroundBrushPresenter != null)
+                BackgroundBrushPresenter.Visibility = backgroundBrushPresenterVisibility;
+
+            if (InactiveBackgroundBrushPresenter != null)
+                InactiveBackgroundBrushPresenter.Visibility = backgroundInactiveBrushPresenterVisibility;
+        }
+
+        private void XamlBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationFrame == null)
+                OnBackRequested();
+            else if (NavigationFrame.CanGoBack)
+                NavigationFrame.GoBack();
+        }
+        #endregion
+    }
+}
--- QKitBlankApp/MainPage.xaml.cs
+++ QKitBlankApp/MainPage.xaml.cs
@@ -3,13 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Composition;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -28,9 +34,45 @@ namespace QKitBlankApp
             this.InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            ApplyAcrylicAccent(RootPanel);
+            RootPanel.SizeChanged += RootPanel_SizeChanged;
+        }
+
+        private void RootPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _hostSprite.Size = new Vector2((float)(sender as Panel).ActualWidth, (float)(sender as Panel).ActualHeight);
+        }
+
+        private void ApplyAcrylicAccent(Panel panel)
+        {
+            _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            _hostSprite = _compositor.CreateSpriteVisual();
+            _hostSprite.Size = new Vector2((float)panel.ActualWidth, (float)panel.ActualHeight);
+
+            ElementCompositionPreview.SetElementChildVisual(panel, _hostSprite);
+            _hostSprite.Brush = _compositor.CreateHostBackdropBrush();
+        }
+        Compositor _compositor;
+        SpriteVisual _hostSprite;
+
         private void GroupTestButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(GroupingView));
+        }
+
+        private void BackButtonToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = ((sender as ToggleButton)?.IsChecked ?? false)
+                ? AppViewBackButtonVisibility.Visible
+                : AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private async void CustomTitle_BackRequested(object sender, EventArgs e)
+        {
+            await new MessageDialog("back requested").ShowAsync();
         }
     }
 }
